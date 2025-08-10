9af30978b4ee71b71dea5732673954507b817c07
Zero-Null <1987617643@qq.com>
2025-08-11 01:46:54 +0800

    首次提交

---
+++ src/main/java/com/heypixel/heypixelmod/obsoverlay/modules/impl/move/NoSlow.java
@@ -0,0 +1,25 @@
+package com.heypixel.heypixelmod.obsoverlay.modules.impl.move;
+
+import com.heypixel.heypixelmod.obsoverlay.events.api.EventTarget;
+import com.heypixel.heypixelmod.obsoverlay.events.impl.EventSlowdown;
+import com.heypixel.heypixelmod.obsoverlay.modules.Category;
+import com.heypixel.heypixelmod.obsoverlay.modules.Module;
+import com.heypixel.heypixelmod.obsoverlay.modules.ModuleInfo;
+import com.heypixel.heypixelmod.obsoverlay.values.ValueBuilder;
+import com.heypixel.heypixelmod.obsoverlay.values.impl.BooleanValue;
+import com.heypixel.heypixelmod.obsoverlay.values.impl.FloatValue;
+
+@ModuleInfo(
+   name = "NoSlow",
+   description = "NoSlowDown",
+   category = Category.MOVEMENT
+)
+public class NoSlow extends Module {
+   @EventTarget
+   public void onSlow(EventSlowdown eventSlowdown) {
+      if (mc.player.getUseItemRemainingTicks() % 2 != 0 && mc.player.getUseItemRemainingTicks() <= 30) {
+         eventSlowdown.setSlowdown(false);
+         mc.player.setSprinting(true);
+      }
+   }
+}
